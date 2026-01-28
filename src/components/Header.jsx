3afaa923b8b5e75ddde22fee7923ca03e4f8e470
pxlblink <249895803+pxlblink@users.noreply.github.com>
2026-01-28 18:31:32 +0300

    Clean up and minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,18 +13,18 @@ export default function Header() {
 
     return (
       <div
-        className="flex items-center h-full border border-green-800"
+        className="flex items-center h-full"
         onMouseEnter={() => setLinkHovered(true)}
         onMouseLeave={() => setLinkHovered(false)}
       >
-        <a href="" className="border border-red-600">
+        <a href="" className="text-[12px]">
           {linkName}
         </a>
         {openMenu && (
           <div
             onMouseEnter={() => setMenuHovered(true)}
             onMouseLeave={() => setMenuHovered(false)}
-            className="absolute right-0 left-0 top-full bg-green-200"
+            className="absolute right-0 left-0 top-full bg-[rgb(255,255,255)]/80"
           >
             {menuContent}
           </div>
@@ -34,10 +34,9 @@ export default function Header() {
   }
 
   return (
-    <div className="flex relative justify-center bg-[rgba(255, 225, 255, .8)]">
-      <div className="flex gap-5 justify-center items-center border border-red-600">
+    <div className="flex relative justify-center bg-[rgb(255,255,255)]/80">
+      <div className="flex gap-5 justify-center items-center">
         <svg
-          className="border border-yellow-500"
           height="48"
           viewBox="0 0 17 48"
           width="17"
@@ -46,7 +45,7 @@ export default function Header() {
           <path d="m15.5752 19.0792a4.2055 4.2055 0 0 0 -2.01 3.5376 4.0931 4.0931 0 0 0 2.4908 3.7542 9.7779 9.7779 0 0 1 -1.2755 2.6351c-.7941 1.1431-1.6244 2.2862-2.8878 2.2862s-1.5883-.734-3.0443-.734c-1.42 0-1.9252.7581-3.08.7581s-1.9611-1.0589-2.8876-2.3584a11.3987 11.3987 0 0 1 -1.9373-6.1487c0-3.61 2.3464-5.523 4.6566-5.523 1.2274 0 2.25.8062 3.02.8062.734 0 1.8771-.8543 3.2729-.8543a4.3778 4.3778 0 0 1 3.6822 1.841zm-6.8586-2.0456a1.3865 1.3865 0 0 1 -.2527-.024 1.6557 1.6557 0 0 1 -.0361-.337 4.0341 4.0341 0 0 1 1.0228-2.5148 4.1571 4.1571 0 0 1 2.7314-1.4078 1.7815 1.7815 0 0 1 .0361.373 4.1487 4.1487 0 0 1 -.9867 2.587 3.6039 3.6039 0 0 1 -2.5148 1.3236z"></path>
         </svg>
         <NavLinkAndMenu
-          linkName={"store"}
+          linkName={"Store"}
           menuContent={
             <div>
               <p>Store menu</p>
