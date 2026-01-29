7267b718f4777f9158b8f5365c92b5abe84d9b5d
pxlblink <249895803+pxlblink@users.noreply.github.com>
2026-01-29 09:27:08 +0300

    Change hard coded tailwind styling to their builtin equivalent

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -12,20 +12,20 @@ export default function HeroBanner({
       }}
       className="mb-3 bg-bottom bg-no-repeat bg-cover"
     >
-      <div className="flex flex-col items-center flex-border pt-[28px] pb-[320px]">
+      <div className="flex flex-col items-center pt-7 pb-80 flex-border">
         <h2 className="font-semibold text-[32px] text-[#1d1d1f]">{header}</h2>
         <p className="leading-tight text-center text-balance text-[19px] text-[#1d1d1f]">
           {subHeader}
         </p>
         <div className="flex gap-3 mt-4">
           <a
-            className="inline-block text-white bg-blue-600 rounded-full py-[7px] text-[14px] px-[15px]"
+            className="inline-block text-white bg-blue-600 rounded-full py-1.75 text-[14px] px-3.75"
             href=""
           >
             {btnOneText}
           </a>
           <a
-            className="inline-block text-blue-600 bg-white rounded-full border borer-blue-600 py-[7px] text-[14px] px-[15px]"
+            className="inline-block text-blue-600 bg-white rounded-full border borer-blue-600 py-1.75 text-[14px] px-3.75"
             href=""
           >
             {btnTwoText}
