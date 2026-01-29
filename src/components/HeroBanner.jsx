b439f0ce39f22d391e17e05588d5fa7d6268f43e
pxlblink <249895803+pxlblink@users.noreply.github.com>
2026-01-29 09:23:38 +0300

    Make hero banner reusable

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,28 +1,34 @@
-export default function HeroBanner() {
+export default function HeroBanner({
+  header,
+  subHeader,
+  btnOneText,
+  btnTwoText,
+  bgImgUrl,
+}) {
   return (
     <div
-      // className="bg-[url('./assets/images/heroes/iphone-family/hero_iphone_family__fuz5j2v5xx6y_medium.jpg')]
-      className="bg-[url('./assets/images/heroes/iphone-family/hero_iphone_family__fuz5j2v5xx6y_small.jpg')]
-        bg-cover bg-bottom bg-no-repeat bg-amber-300 border-2 border-amber-600"
+      style={{
+        backgroundImage: `url('${bgImgUrl}')`,
+      }}
+      className="mb-3 bg-bottom bg-no-repeat bg-cover"
     >
-      <div className="flex flex-col items-center border-blue-800 flex-border mt-[28px] mb-[320px]">
-        <h2 className="font-semibold text-[32px] text-[#1d1d1f]">iPhone</h2>
-        <p className="leading-tight text-center text-balance mx-30 text-[19px] text-[#1d1d1f]">
-          Say Hello to the latest generation of iPhone.
+      <div className="flex flex-col items-center flex-border pt-[28px] pb-[320px]">
+        <h2 className="font-semibold text-[32px] text-[#1d1d1f]">{header}</h2>
+        <p className="leading-tight text-center text-balance text-[19px] text-[#1d1d1f]">
+          {subHeader}
         </p>
         <div className="flex gap-3 mt-4">
           <a
             className="inline-block text-white bg-blue-600 rounded-full py-[7px] text-[14px] px-[15px]"
             href=""
           >
-            Learn more
+            {btnOneText}
           </a>
           <a
-            // className="inline-block py-2 px-5 text-blue-600 bg-white rounded-full border border-blue-600"
-            className="inline-block text-white bg-blue-600 rounded-full py-[7px] text-[14px] px-[15px]"
+            className="inline-block text-blue-600 bg-white rounded-full border borer-blue-600 py-[7px] text-[14px] px-[15px]"
             href=""
           >
-            Shop iPhone
+            {btnTwoText}
           </a>
         </div>
       </div>
