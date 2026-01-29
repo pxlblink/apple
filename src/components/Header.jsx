660a358bbbf02044999f133c102082e1a9f576d6
pxlblink <249895803+pxlblink@users.noreply.github.com>
2026-01-29 10:50:43 +0300

    Add blur to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ export default function Header() {
   }
 
   return (
-    <div className="flex sticky top-0 justify-center w-full bg-[rgb(255,255,255)]/80">
+    <div className="flex sticky top-0 justify-center w-full backdrop-blur-md bg-[rgb(255,255,255)]/80">
       <div className="flex gap-5 justify-center items-center">
         <svg
           height="48"
