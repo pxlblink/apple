da3cb246efca8ac7fd0f1febf5abbe41aed34b8f
pxlblink <249895803+pxlblink@users.noreply.github.com>
2026-01-29 20:09:15 +0300

    Create Accordion component

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function Accordion({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="py-2 border-b border-[rgba(255,255,255,0.24)]">
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="flex justify-between w-full cursor-pointer text-[rgba(255,255,255,0.92)] text-[.8rem]"
+      >
+        <span>{title}</span>
+        {isOpen ? <span>-</span> : <span>+</span>}
+      </button>
+      <div
+        className={`grid overflow-hidden
+          ${
+            isOpen ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
+          }`}
+      >
+        <div className="overflow-hidden px-4 text-[rgba(255,255,255,0.8)]">
+          {content}
+        </div>
+      </div>
+    </div>
+  );
+}
