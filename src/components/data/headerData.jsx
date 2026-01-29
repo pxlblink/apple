20bcc487bcfe3c63cf14033af556da182d62f9bc
pxlblink <249895803+pxlblink@users.noreply.github.com>
2026-01-29 13:22:06 +0300

    Make a file to store navbar menu data

---
+++ src/components/data/headerData.jsx
@@ -0,0 +1,96 @@
+export default [
+  {
+    name: "Store",
+    menu: {
+      sections: [
+        {
+          title: "Shop",
+          largeContent: [
+            "Shop the latest",
+            "Mac",
+            "iPad",
+            "iPhone",
+            "Apple Watch",
+            "Apple Vision Pro",
+            "AirPods",
+            "Accessories",
+          ],
+          smallContent: [],
+        },
+        {
+          title: "Quick Links",
+          largeContent: [],
+          smallContent: [
+            "Find a Store",
+            "Order Status",
+            "Apple Trade In",
+            "Financing",
+            "Personal Setup",
+          ],
+        },
+        {
+          title: "Shop Special Stores",
+          largeContent: [],
+          smallContent: [
+            "Certified Refubrished",
+            "Education",
+            "Business",
+            "Veterans and Military",
+            "Government",
+          ],
+        },
+      ],
+    },
+  },
+  {
+    name: "Mac",
+    menu: {
+      sections: [
+        {
+          title: "Explore Mac",
+          largeContent: [
+            "Explore All Mac",
+            "MacBook Pro",
+            "iMac",
+            "Mac mini",
+            "Mac Studio",
+            "Mac Pro",
+            "Displays",
+          ],
+          smallContent: [
+            "Compare mac",
+            "Switch from PC to Mac",
+          ],
+        },
+        {
+          title: "Shop Mac",
+          largeContent: [],
+          smallContent: [
+            "Shop Mac",
+            "Help Me Choose",
+            "Mac Accessories",
+            "Apple Trade In",
+            "Financing",
+            "Personal Setup"
+          ],
+        },
+        {
+          title: "More from Mac",
+          largeContent: [],
+          smallContent: [
+            "Mac Support",
+            "AppleCare",
+            "macOS Tahoe",
+            "Apple Intelligence",
+            "Apps by Apple",
+            "Apple Creator Studio",
+            "Better with iPhone",
+            "iCloud+",
+            "Mac for Business",
+            "Education",
+          ],
+        },
+      ],
+    },
+  },
+];
