ecf24ca8dd636ec17cfbb4aff985b7f0b949ea91
pxlblink <249895803+pxlblink@users.noreply.github.com>
2026-01-31 12:48:20 +0300

    Make navigation work on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,17 +34,85 @@ export default function Header() {
     );
   }
 
+  const [mobileNavIsOpen, setMobileNavIsOpen] = useState(false);
+
+  function MobileNav() {
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+    const [currentMenu, setCurrentMenu] = useState(null);
+
+    return (
+      <div
+        className={`${mobileNavIsOpen ? "block" : "hidden"} md:hidden fixed right-0 left-0 pt-15 bg-gray-50 -z-1 h-svh`}
+      >
+        {menuContent.map((content, i) => (
+          <div key={i} className="flex flex-col pl-10">
+            {!menuIsOpen && (
+              <p
+                onClick={() => {
+                  setCurrentMenu(content.menu);
+                  setMenuIsOpen(true);
+                }}
+                className="block mb-1 font-semibold cursor-pointer text-[1.6rem]"
+              >
+                {content.name}
+              </p>
+            )}
+          </div>
+        ))}
+        {menuIsOpen && (
+          <div>
+            <svg
+              className="fixed top-0 left-2 w-4 cursor-pointer"
+              onClick={() => setMenuIsOpen(false)}
+              height="48"
+              viewBox="0 0 9 48"
+              width="9"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="m1.5618 24.0621 6.5581-6.4238c.2368-.2319.2407-.6118.0088-.8486-.2324-.2373-.6123-.2407-.8486-.0088l-7 6.8569c-.1157.1138-.1807.2695-.1802.4316.001.1621.0674.3174.1846.4297l7 6.7241c.1162.1118.2661.1675.4155.1675.1577 0 .3149-.062.4326-.1846.2295-.2388.2222-.6187-.0171-.8481z"></path>
+            </svg>
+            <div className="overflow-y-scroll pl-10 max-h-screen bg-gray-50 no-scrollbar">
+              {currentMenu.sections.map((menuData, i) => (
+                <div key={i}>
+                  {i !== 0 && (
+                    <p className="mt-10 mb-4 text-gray-500" key={i}>
+                      {menuData.title}
+                    </p>
+                  )}
+                  {console.log(menuData.largeContent)}
+                  {menuData.largeContent.map((link, i) => (
+                    <p className="mb-1 text-2xl font-semibold" key={i}>
+                      {link}
+                    </p>
+                  ))}
+                  {menuData.smallContent.map((link, i) => (
+                    <p className="text-lg font-semibold" key={i}>
+                      {link}
+                    </p>
+                  ))}
+                </div>
+              ))}
+              <div className="w-full h-40"></div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+
   return (
     <div className="sticky top-0 w-full backdrop-blur-md bg-[rgb(255,255,255)]/80">
-      <div className="flex justify-between px-5 md:gap-3 md:justify-center lg:gap-6">
-        <svg
-          height="48"
-          viewBox="0 0 17 48"
-          width="17"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="m15.5752 19.0792a4.2055 4.2055 0 0 0 -2.01 3.5376 4.0931 4.0931 0 0 0 2.4908 3.7542 9.7779 9.7779 0 0 1 -1.2755 2.6351c-.7941 1.1431-1.6244 2.2862-2.8878 2.2862s-1.5883-.734-3.0443-.734c-1.42 0-1.9252.7581-3.08.7581s-1.9611-1.0589-2.8876-2.3584a11.3987 11.3987 0 0 1 -1.9373-6.1487c0-3.61 2.3464-5.523 4.6566-5.523 1.2274 0 2.25.8062 3.02.8062.734 0 1.8771-.8543 3.2729-.8543a4.3778 4.3778 0 0 1 3.6822 1.841zm-6.8586-2.0456a1.3865 1.3865 0 0 1 -.2527-.024 1.6557 1.6557 0 0 1 -.0361-.337 4.0341 4.0341 0 0 1 1.0228-2.5148 4.1571 4.1571 0 0 1 2.7314-1.4078 1.7815 1.7815 0 0 1 .0361.373 4.1487 4.1487 0 0 1 -.9867 2.587 3.6039 3.6039 0 0 1 -2.5148 1.3236z"></path>
-        </svg>
+      <div className="flex justify-between px-5 md:gap-3 md:justify-center lg:gap-6 isolate">
+        {!mobileNavIsOpen && (
+          <svg
+            height="48"
+            viewBox="0 0 17 48"
+            width="17"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="m15.5752 19.0792a4.2055 4.2055 0 0 0 -2.01 3.5376 4.0931 4.0931 0 0 0 2.4908 3.7542 9.7779 9.7779 0 0 1 -1.2755 2.6351c-.7941 1.1431-1.6244 2.2862-2.8878 2.2862s-1.5883-.734-3.0443-.734c-1.42 0-1.9252.7581-3.08.7581s-1.9611-1.0589-2.8876-2.3584a11.3987 11.3987 0 0 1 -1.9373-6.1487c0-3.61 2.3464-5.523 4.6566-5.523 1.2274 0 2.25.8062 3.02.8062.734 0 1.8771-.8543 3.2729-.8543a4.3778 4.3778 0 0 1 3.6822 1.841zm-6.8586-2.0456a1.3865 1.3865 0 0 1 -.2527-.024 1.6557 1.6557 0 0 1 -.0361-.337 4.0341 4.0341 0 0 1 1.0228-2.5148 4.1571 4.1571 0 0 1 2.7314-1.4078 1.7815 1.7815 0 0 1 .0361.373 4.1487 4.1487 0 0 1 -.9867 2.587 3.6039 3.6039 0 0 1 -2.5148 1.3236z"></path>
+          </svg>
+        )}
         <div className="flex gap-1 lg:gap-3">
           {menuContent.map((content, i) => (
             <NavLinkAndMenu
@@ -77,26 +145,37 @@ export default function Header() {
               }
             />
           ))}
+          {<MobileNav />}
         </div>
         <div className="flex gap-8 items-center">
+          {!mobileNavIsOpen && (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="15px"
+                height="44px"
+                viewBox="0 0 15 44"
+              >
+                <path d="M14.298,27.202l-3.87-3.87c0.701-0.929,1.122-2.081,1.122-3.332c0-3.06-2.489-5.55-5.55-5.55c-3.06,0-5.55,2.49-5.55,5.55 c0,3.061,2.49,5.55,5.55,5.55c1.251,0,2.403-0.421,3.332-1.122l3.87,3.87c0.151,0.151,0.35,0.228,0.548,0.228 s0.396-0.076,0.548-0.228C14.601,27.995,14.601,27.505,14.298,27.202z M1.55,20c0-2.454,1.997-4.45,4.45-4.45 c2.454,0,4.45,1.997,4.45,4.45S8.454,24.45,6,24.45C3.546,24.45,1.55,22.454,1.55,20z"></path>
+              </svg>
+              <svg
+                className="md:hidden"
+                height="44"
+                viewBox="0 0 14 44"
+                width="14"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="m11.3535 16.0283h-1.0205a3.4229 3.4229 0 0 0 -3.333-2.9648 3.4229 3.4229 0 0 0 -3.333 2.9648h-1.02a2.1184 2.1184 0 0 0 -2.117 2.1162v7.7155a2.1186 2.1186 0 0 0 2.1162 2.1167h8.707a2.1186 2.1186 0 0 0 2.1168-2.1167v-7.7155a2.1184 2.1184 0 0 0 -2.1165-2.1162zm-4.3535-1.8652a2.3169 2.3169 0 0 1 2.2222 1.8652h-4.4444a2.3169 2.3169 0 0 1 2.2222-1.8652zm5.37 11.6969a1.0182 1.0182 0 0 1 -1.0166 1.0171h-8.7069a1.0182 1.0182 0 0 1 -1.0165-1.0171v-7.7155a1.0178 1.0178 0 0 1 1.0166-1.0166h8.707a1.0178 1.0178 0 0 1 1.0164 1.0166z"></path>
+              </svg>
+            </>
+          )}
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="15px"
-            height="44px"
-            viewBox="0 0 15 44"
+            onClick={() => setMobileNavIsOpen((prev) => !prev)}
+            className="cursor-pointer md:hidden"
+            width="18"
+            height="48"
+            viewBox="0 0 18 18"
           >
-            <path d="M14.298,27.202l-3.87-3.87c0.701-0.929,1.122-2.081,1.122-3.332c0-3.06-2.489-5.55-5.55-5.55c-3.06,0-5.55,2.49-5.55,5.55 c0,3.061,2.49,5.55,5.55,5.55c1.251,0,2.403-0.421,3.332-1.122l3.87,3.87c0.151,0.151,0.35,0.228,0.548,0.228 s0.396-0.076,0.548-0.228C14.601,27.995,14.601,27.505,14.298,27.202z M1.55,20c0-2.454,1.997-4.45,4.45-4.45 c2.454,0,4.45,1.997,4.45,4.45S8.454,24.45,6,24.45C3.546,24.45,1.55,22.454,1.55,20z"></path>
-          </svg>
-          <svg
-            className="md:hidden"
-            height="44"
-            viewBox="0 0 14 44"
-            width="14"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="m11.3535 16.0283h-1.0205a3.4229 3.4229 0 0 0 -3.333-2.9648 3.4229 3.4229 0 0 0 -3.333 2.9648h-1.02a2.1184 2.1184 0 0 0 -2.117 2.1162v7.7155a2.1186 2.1186 0 0 0 2.1162 2.1167h8.707a2.1186 2.1186 0 0 0 2.1168-2.1167v-7.7155a2.1184 2.1184 0 0 0 -2.1165-2.1162zm-4.3535-1.8652a2.3169 2.3169 0 0 1 2.2222 1.8652h-4.4444a2.3169 2.3169 0 0 1 2.2222-1.8652zm5.37 11.6969a1.0182 1.0182 0 0 1 -1.0166 1.0171h-8.7069a1.0182 1.0182 0 0 1 -1.0165-1.0171v-7.7155a1.0178 1.0178 0 0 1 1.0166-1.0166h8.707a1.0178 1.0178 0 0 1 1.0164 1.0166z"></path>
-          </svg>
-          <svg className="md:hidden" width="18" height="18" viewBox="0 0 18 18">
             <polyline
               id="globalnav-menutrigger-bread-bottom"
               fill="none"
